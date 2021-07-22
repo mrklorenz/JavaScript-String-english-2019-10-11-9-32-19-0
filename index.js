b96748c66a024b8af9e4790553fed421c62d44e3
balajma <mark.lorenz.balajadia@oocl.com>
2021-07-22 15:36:08 +0800

    Added answers to index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+name = name.toUpperCase();
+console.log(name);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+var array = sentence.split(" ");
+for (let i = 0; i < newarray.length; i++) {
+  array[i] = array[i].charAt(0).toUpperCase() + array[i].substr(1);
+}
+console.log(array.join(" "));
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+money = money.substr(1,2);
+console.log(money);
